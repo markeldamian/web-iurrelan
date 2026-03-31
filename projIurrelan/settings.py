7a0d66aa0b7a697c198d22a4a94e90d2b673c47a
markel.damian <markel.damian@opendeusto.es>
2026-03-31 18:21:10 +0200

    Añadidos gunicorn y whitenoise para Railway

--- projIurrelan/settings.py
+++ projIurrelan/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--9xgg76o49j!rc!^-%emt9l7m#5w5xq@*=_(pd+m$#3c1%ur^_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['web-iurrelan-2.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['iurrelan.com','www.iurrelan.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
